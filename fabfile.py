a637197f7c03c5e5358e5001f9c7a85ece45653d
Ian Dennis Miller <ian@iandennismiller.com>
2011-12-21 15:40:58 -0500

    fixing git_put

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import sys, os
 from fabric.api import env, run, put, open_shell, local
 
-# step 1: git clone git@github.com:iandennismiller/droptato.git
+# step 1: git clone git@github.com:iandennismiller/droptato.git ./somewhere
 
 # step 2: edit these SSH values
 
@@ -9,7 +9,7 @@ env.user = ''
 env.key_filename = ['']
 env.hosts = ['']
 
-# step 3: fab init
+# step 3: cd somewhere; fab shell; fab init
 
 def help():
     "print usage information"
@@ -57,11 +57,12 @@ def hook_install():
 def git_put():
     "create new master repository based on this one"
     
-    cmd = "git clone --bare . /tmp/droptato.git"
+    local("git clone --bare . /tmp/droptato.git")
     put("/tmp/droptato.git","/home/private")
-    cmd = "git remote add origin %s@%s/home/private/droptato.git" % (env.user, env.hosts[0])
-    cmd = "git push -u origin master"
     os.system("rm -rf /tmp/droptato.git")
+    local("git remote rm origin; git remote add origin %s@%s:/home/private/droptato.git" % (env.user, env.hosts[0]))
+    local("git push -u origin master")
+    help()
 
 def git_init():
     "initialize an empty remote master repository: droptato.git"
